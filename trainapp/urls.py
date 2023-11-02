3660e12d46127af6b40b2d1732a6ad8d0a6aca6c
Eric Wang <ericbad1234@gmil.com>
2023-11-02 16:37:00 +0800

    建立index頁面

--- trainapp/urls.py
+++ trainapp/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path 
+from django.urls import path
 from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('books/',views.get_books),
-    path('',views.index),
-    path('lottory/',views.get_lottory2)
+    path('books/', views.get_books),
+    path('', views.index),
+    # path('lottory/',views.get_lottory2)
 ]
