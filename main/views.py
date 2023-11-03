4194a6f24e7c1347bf5ac711fc255974e0ec5296
Eric Wang <ericbad1234@gmil.com>
2023-11-03 21:40:53 +0800

    添加爬蟲後的html顯示模板

--- main/views.py
+++ main/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from tools import get_chrome, getSoup
+from bs4 import BeautifulSoup
+import requests
+from selenium.webdriver.common.by import By
 import json
 from datetime import datetime
 import time
 import random
+from threading import Thread
 # Create your views here.
+url = 'https://www.manhuaren.com/search/'
 
 
 def get_books(request):
@@ -18,7 +23,39 @@ def get_books(request):
 
 
 def index(request):
-    return render(request, 'index.html')
+    global url
+    datas = quick_pick_comic(url)
+    context = {}
+    for data in datas:
+        context['title'] = data[0]
+        context['new_update'] = data[1]
+        context['comic_url'] = data[2]
+        context['img_url'] = data[-1]
+        print(context)
+        return render(request, 'index.html', locals())
+
+
+def quick_pick_comic(url):
+    datas = []
+    chrome = get_chrome(url)
+    comic_xpath = '/html/body/ul[1]/li[1]/a'
+    chrome.find_element(By.XPATH, comic_xpath).click()
+    soup = BeautifulSoup(chrome.page_source, 'lxml')
+    if soup != None:
+        content = soup.find('ul', class_="manga-list-2").findAll('li')
+    try:
+        for comic in content:
+            title = [title.text.strip() for title in comic][3]
+            new_update = [title.text.strip() for title in comic][-2]
+            comic_url = 'https://www.manhuaren.com'+comic.find('a').get('href')
+            img_url = comic.find('img').get('src')
+            datas.append([title, new_update, comic_url, img_url])
+    except Exception as e:
+        print(e)
+    finally:
+        if chrome != None:
+            chrome.close()
+    return datas
 
 
 def get_lottory(request):
